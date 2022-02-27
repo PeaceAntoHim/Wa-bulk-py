90d507f6529dea1b17910fb647216b85fbc9b1ae
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-27 12:10:38 +0700

    again

---
+++ src/config.py
@@ -0,0 +1,6 @@
+CHROME_PROFILE_PATH = "C:\\Users\\FRANSS~1\\AppData\\Local\\Temp\\scoped_dir31412_443101046\\Default"
+# CHROME_PROFILE_PATH = "user-data-dir=Users/akjasim/Library/Application Support/Google/Chrome/Wtsp"
+
+# Windows 7, 8.1, and 10: C:\Users\<username>\AppData\Local\Google\Chrome\User Data\Default
+# Mac OS X El Capitan: Users/<username>/Library/Application Support/Google/Chrome/Default
+# Linux: /home/<username>/.config/google-chrome/default
